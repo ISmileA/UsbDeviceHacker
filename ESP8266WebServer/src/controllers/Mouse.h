57b4f5f7cff80973531b6d9288658fd49be47d9c
Arseniy <120576678+IgnatevArseniy@users.noreply.github.com>
2024-10-11 15:40:35 +0300

    добавил ArduinoJson.h

--- ESP8266WebServer/src/controllers/Mouse.h
+++ ESP8266WebServer/src/controllers/Mouse.h
@@ -1,7 +1,17 @@
 #pragma once
+
 #include "main.h"
+#include "ArduinoJson.h"
 
 namespace Mouse{
-    void set(){};
+    void set(){
+        if(server.method() == HTTP_POST){
+            StaticJsonDocument<300> parsed;
+            DeserializationError error = deserializeJson(parsed, server.arg("plain"));
+            if(!error){
+                server.send(200, "text/html", "NiceJson");
+            }
+        }
+    };
     void remove(){};
 }
